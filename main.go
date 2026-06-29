f0c87d47e59580d89abd82f86c7408bf4d79da8b
thedasken <thedasken@proton.me>
2026-06-29 13:01:39 +0200

    refactor: Run func

--- main.go
+++ main.go
@@ -21,55 +21,19 @@ type Result struct {
 }
 
 var numbers = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-var workers = 3
+var workersCount = 3
 
 func main() {
-	// buffered channels
 	jobs := make(chan Job, len(numbers))
-	results := make(chan Result, len(numbers))
-
-	var wg sync.WaitGroup
 
 	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
 	defer cancel()
 
-	// start workers
-	for i := 1; i <= workers; i++ {
-		wg.Add(1)
-
-		go func(workerID int) {
-			defer wg.Done()
-			worker(ctx, workerID, jobs, results)
-		}(i)
-	}
-
-	// send jobs
-	go func() {
-		defer close(jobs)
-		for i, v := range numbers {
-			job := Job{
-				ID:    i,
-				Value: v,
-			}
-			jobs <- job
-		}
-	}()
-
-	// close results when workers are done
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
+	resultsByJobID := Run(ctx, jobs, workersCount, len(numbers))
 
 	successCount := 0
 	errorCount := 0
 	skippedCount := 0
-
-	resultsByJobID := make([]Result, len(numbers))
-
-	for res := range results {
-		resultsByJobID[res.JobID] = res
-	}
 
 	for jobID, res := range resultsByJobID {
 		if !res.Done {
@@ -154,4 +118,34 @@ func process(ctx context.Context, job Job) (int, error) {
 	}
 
 	return job.Value * 2, nil
+}
+
+func Run(ctx context.Context, jobs <-chan Job, workersCount int, jobCount int) []Result {
+	results := make(chan Result, jobCount)
+
+	var wg sync.WaitGroup
+
+	// start workers
+	for i := 1; i <= workersCount; i++ {
+		wg.Add(1)
+
+		go func(workerID int) {
+			defer wg.Done()
+			worker(ctx, workerID, jobs, results)
+		}(i)
+	}
+
+	// close results when workers are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	resultsByJobID := make([]Result, jobCount)
+
+	for res := range results {
+		resultsByJobID[res.JobID] = res
+	}
+
+	return resultsByJobID
 }
